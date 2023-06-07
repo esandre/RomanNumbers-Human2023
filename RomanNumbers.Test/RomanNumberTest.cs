88e497b58e1b678dbda217b16a56d77595a49248
Enzo Sandré <atelier@enzosandre.fr>
2023-06-07 14:01:14 +0200

    Cinquième test

--- RomanNumbers.Test/RomanNumberTest.cs
+++ RomanNumbers.Test/RomanNumberTest.cs
@@ -30,4 +30,19 @@ public class RomanNumberTest
         // ALORS j'obtiens IV
         Assert.Equal("IV", nombreRomain);
     }
+
+    [Fact(DisplayName = "ETANT DONNE le chiffre 5 " +
+                         "QUAND je le convertis en nombres romains " +
+                         "ALORS j'obtiens V")]
+    public void TestCinq()
+    {
+        // ETANT DONNE le chiffre 5
+        const uint chiffreArabe = 5;
+
+        // QUAND je le convertis en nombres romains
+        var nombreRomain = ConvertisseurNombresRomains.Convertir(chiffreArabe);
+
+        // ALORS j'obtiens V
+        Assert.Equal("V", nombreRomain);
+    }
 }
--- RomanNumbers/ConvertisseurNombresRomains.cs
+++ RomanNumbers/ConvertisseurNombresRomains.cs
@@ -4,11 +4,14 @@ public static class ConvertisseurNombresRomains
 {
     public static string Convertir(uint chiffreArabe)
     {
-        if (chiffreArabe <= 3)
-        {
-            return new string('I', (int)chiffreArabe);
-        }
-        
-        return "IV";
+        if (chiffreArabe < 5)
+            switch (chiffreArabe)
+            {
+                case <= 3:
+                    return new string('I', (int)chiffreArabe);
+                default:
+                    return "IV";
+            }
+        else return "V";
     }
 }
