1037c14af1e0e30a0a8bd90e3ba3fcefc55b86ac
Enzo Sandré <atelier@enzosandre.fr>
2023-07-07 08:43:17 +0200

    Refacto du test

--- RomanNumbers.Test/RomanNumberTest.cs
+++ RomanNumbers.Test/RomanNumberTest.cs
@@ -40,18 +40,23 @@ public class RomanNumberTest
         Assert.Equal("IX", nombreRomain);
     }
 
+    public static readonly IEnumerable<object[]> CasSymbolePlusUnité = new[]
+    {
+        new object[] { 'V', "test", 5 },
+        new object[] { 'V', 5, 6 },
+        new object[] { 'V', 5, 7 },
+        new object[] { 'V', 5, 8 },
+        new object[] { 'X', 10, 10 },
+        new object[] { 'X', 10, 11 },
+        new object[] { 'X', 10, 12 },
+        new object[] { 'X', 10, 13 }
+    };
+
     [Theory(DisplayName = "ETANT DONNE un symbole <symbole> valant <valeurDeBase> " +
                           "ET un nombre <nombreUnités> compris entre 0 et 3 au-dessus de cette valeur " +
                           "QUAND je le convertis en nombres romains " +
                           "ALORS j'obtiens <symbole> plus <(nombreUnités-valeurDeBase)> fois I")]
-    [InlineData('V', 5, 5)]
-    [InlineData('V', 5, 6)]
-    [InlineData('V', 5, 7)]
-    [InlineData('V', 5, 8)]
-    [InlineData('X', 10, 10)]
-    [InlineData('X', 10, 11)]
-    [InlineData('X', 10, 12)]
-    [InlineData('X', 10, 13)]
+    [MemberData(nameof(CasSymbolePlusUnité))]
     public void TestSymbolePlusUnité(char symbole, uint valeurDeBase, uint nombreUnités)
     {
         var nombreRomain = ConvertisseurNombresRomains.Convertir(nombreUnités);
